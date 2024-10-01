a39291502faab3a9d87c50fbeb63f85c3a765c5b
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2024-10-01 18:56:37 -0300

    Delete PetShopGUI directory
    duplicata de arquivos

--- PetShopGUI/ListarAnimaisGUI.java
+++
@@ -1,6 +0,0 @@
-public class ListarAnimaisGUI {
-
-    public void mostrarTelaListarAnimais() {
-
-    }
-}
--- PetShopGUI/ListarClientesGUI.java
+++
@@ -1,6 +0,0 @@
-public class ListarClientesGUI {
-
-    public void mostrarTelaListarClientes() {
-
-    }
-}
